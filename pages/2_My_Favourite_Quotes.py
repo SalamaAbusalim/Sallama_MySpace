47a5f92e04c3c2a0087ce95ed578d9a074002b96
Sallama <si.abusalim@gmail.com>
2024-01-03 21:58:22 +0200

    add pages

--- pages/2_My_Favourite_Quotes.py
+++ pages/2_My_Favourite_Quotes.py
@@ -1,6 +1,13 @@
 import streamlit as st
+st.set_page_config(layout="wide")
 
-
-button = st.button("Open the Quote", type= "primary")
+col2, col2, col3 = st.columns([3,1.7,3])
+col4, col5, col6 = st.columns([1,4,1])
+with col2:
+    button = st.button("Open the Quote", type= "primary")
 if button:
-    st.title("سددوا وقاربوا وأبشروا..واعلموا أن أحب الأعمال الى الله أدومها وإن قل")
+    with col5:
+        st.header("")
+        st.header("سددوا وقاربوا وأبشروا.. واعلموا أن أحب الأعمال الى الله أدومها وإن قل")
+
+
